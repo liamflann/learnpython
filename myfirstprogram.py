918c9bb80092220a678ca855eae4b8c862dd954c
Liam Flannagan <William.Flannagan@UCDenver.edu>
2016-10-17 13:52:14 -0600

    I want to add this to github

---
+++ myfirstprogram.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 13:39:18 2016
+
+@author: Liam
+"""
+
+name = raw_input('What is your name? ')
+print'Hello', name
+print"Welcome to Python", name
